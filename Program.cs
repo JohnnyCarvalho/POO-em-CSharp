14e24d481621f59c8f1e9e3a54394d10b8eb9482
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-03-14 00:07:33 -0300

    Subindo pastas

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace POO_em_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Escreva seu nome: ");Console.ReadLine();
+        }
+    }
+}
